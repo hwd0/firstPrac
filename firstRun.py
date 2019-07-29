4783c3869cfd90b1d9a59476bcca92890abf5d3c
hamza <wldham001@myuct.ac.za>
2019-07-29 13:41:34 +0200

    LED should blink

--- firstRun.py
+++ firstRun.py
@@ -7,6 +7,11 @@ GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(3, GPIO.OUT, initial=GPIO.HIGH)
 
+while True:
+	GPIO.output(3, GPIO.LOW)
+	time.sleep(0.5)
+	GPIO.output(12, GPIO.HIGH)
+	time.sleep(0.5)
 
 
 GPIO.cleanup()
