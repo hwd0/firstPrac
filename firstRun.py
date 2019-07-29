f45ef9b900e138d416080175aebea768e65ce34a
hamza <wldham001@myuct.ac.za>
2019-07-29 14:29:41 +0200

    00 - LED with push button

--- firstRun.py
+++ firstRun.py
@@ -1,19 +1,44 @@
+#!/usr/bin/python3
+"""
+Python Practical Template
+Keegan Crankshaw
+Readjust this Docstring as follows:
+Names: Hamza
+Student Number: wldham001
+Prac: 01
+Date: <29/07/2019>
+"""
+
+# import Relevant Librares
 import RPi.GPIO as GPIO
-import time
 
+GPIO.cleanup()
 
-
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(3, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 GPIO.setwarnings(False)
 
-while True:
-	GPIO.output(3, GPIO.LOW)
-	time.sleep(0.5)
-	GPIO.output(3, GPIO.HIGH)
-	time.sleep(0.5)
+# Logic that you write
+def main():
+    print("write your logic here")
 
 
-GPIO.cleanup()
+# Only run the functions if 
+if __name__ == "__main__":
+    # Make sure the GPIO is stopped correctly
+    try:
+        while True:
+            main()
+	except GPIO.wait_for_edge(5, GPIO.FALLING)	
+		GPIO.output(3, GPIO.HIGH)
+    except KeyboardInterrupt:
+        print("Exiting gracefully")
+        # Turn off your GPIOs here
+        GPIO.cleanup()
+    except Exception as e:
+        GPIO.cleanup()
+        print("Some other error occurred")
+        print(e.message)
