6b443db83fe830152af2ececb7ef23545b3e1968
hamza <wldham001@myuct.ac.za>
2019-07-29 15:07:10 +0200

    03 - LED with push button

--- firstRun.py
+++ firstRun.py
@@ -21,18 +21,20 @@ GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 GPIO.setwarnings(False)
 
+def call(channel):
+	print 'button pressed'
+	GPIO.output(3, GPIO.HIGH)
 # Logic that you write
 def main():
-    print("write your logic here")
-
+    print("waiting for button press")
+	GPIO.add_event_detect(5, GPIO.FALLING, callback=call bouncetime=300)
 
 # Only run the functions if 
 if __name__ == "__main__":
     # Make sure the GPIO is stopped correctly
     try:
-        GPIO.wait_for_edge(5, GPIO.FALLING) 
-		GPIO.output(3, GPIO.HIGH)
-        main()
+        while True:
+			main()
     except KeyboardInterrupt:
         print("Exiting gracefully")
         # Turn off your GPIOs here
