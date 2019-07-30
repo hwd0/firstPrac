4c426cd8e7f0fd2a5a47dec53d80a74072c43479
hamza <wldham001@myuct.ac.za>
2019-07-30 13:37:13 +0200

    02 - button test

--- 2ndRun.py
+++ 2ndRun.py
@@ -14,7 +14,7 @@ def button_callback(channel):
     print("Button was pushed!")
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(39, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.add_event_detect(39,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
+GPIO.setup(39, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.add_event_detect(39,GPIO.FALLING,callback=button_callback) # Setup event on pin 10 rising edge
 message = input("Press enter to quit\n\n") # Run until someone presses enter
 GPIO.cleanup() # Clean up
